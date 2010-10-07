ac90a9e42258b6d152aee4f5ebb2eaac152afeed
frankmorgner <frankmorgner@96b47cad-a561-4643-ad3b-153ac7d7599c>
2010-10-07 13:12:48 +0000

    added copyright and author
    git-svn-id: https://vsmartcard.svn.sourceforge.net/svnroot/vsmartcard@302 96b47cad-a561-4643-ad3b-153ac7d7599c

--- picc_to_pcsc/src/picc_to_pcsc.c
+++ picc_to_pcsc/src/picc_to_pcsc.c
@@ -1,4 +1,21 @@
-#define _GNU_SOURCE
+/*
+ * Copyright (C) 2010 Dominik Oepen, Frank Morgner
+ *
+ * This file is part of picc_to_pcsc.
+ *
+ * picc_to_pcsc is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * picc_to_pcsc is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * picc_to_pcsc.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
--- picc_to_pcsc/src/picc_to_pcsc.h
+++ picc_to_pcsc/src/picc_to_pcsc.h
@@ -1,7 +1,30 @@
+/*
+ * Copyright (C) 2010 Dominik Oepen, Frank Morgner
+ *
+ * This file is part of picc_to_pcsc.
+ *
+ * picc_to_pcsc is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * picc_to_pcsc is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * picc_to_pcsc.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef PICC_TO_PCSC
 #define PICC_TO_PCSC
 
 #define PICCDEV "/dev/ttyACM0"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef void (*sighandler_t)(int);
 
@@ -13,4 +36,7 @@ void setup(void);
 void cleanup(void);
 void printHex(char *title, unsigned char *msg, unsigned int length); 
 
+#ifdef  __cplusplus
+}
+#endif
 #endif
