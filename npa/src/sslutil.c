7c2be6b93c765a2b0de39e6f731c1bf526a52e45
frankmorgner <frankmorgner@96b47cad-a561-4643-ad3b-153ac7d7599c>
2011-09-30 15:41:23 +0000

    implemented ssl_error as macro
    git-svn-id: https://vsmartcard.svn.sourceforge.net/svnroot/vsmartcard@540 96b47cad-a561-4643-ad3b-153ac7d7599c

--- npa/src/sslutil.c
+++ npa/src/sslutil.c
@@ -16,15 +16,3 @@
  * You should have received a copy of the GNU General Public License along with
  * ccid.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "sslutil.h"
-#include <openssl/err.h>
-#include <libopensc/log.h>
-
-void ssl_error(sc_context_t *ctx) {
-    unsigned long r;
-    ERR_load_crypto_strings();
-    for (r = ERR_get_error(); r; r = ERR_get_error()) {
-        sc_debug(ctx, SC_LOG_DEBUG_VERBOSE, ERR_error_string(r, NULL));
-    }
-    ERR_free_strings();
-}
