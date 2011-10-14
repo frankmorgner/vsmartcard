1fc1aa6ca180d9ed33e3b76034d16f03426d17e4
oepen <oepen@96b47cad-a561-4643-ad3b-153ac7d7599c>
2011-10-14 11:19:17 +0000

    Changed favicon
    git-svn-id: https://vsmartcard.svn.sourceforge.net/svnroot/vsmartcard@555 96b47cad-a561-4643-ad3b-153ac7d7599c

--- doc/conf.py
+++ doc/conf.py
@@ -115,7 +115,7 @@ html_logo = "vscard-logo_128.png"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "vscard-logo_128.png" 
+html_favicon = "chip.png" 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
