5db58fec6e99ea4c5254d4ea396a4d5800107ab5
Frank Morgner <frankmorgner@gmail.com>
2023-09-30 03:00:40 +0200

    docs: fixed generated images

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2023-09-27',
+    VERSION: '2023-09-30',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
