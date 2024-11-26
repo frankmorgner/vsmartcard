fcf3e37ae45fe5dc956515c7de9e0a5d0f49408c
Frank Morgner <frankmorgner@gmail.com>
2024-11-26 15:04:12 +0100

    remote-reader: code cleanup

--- remote-reader/app/src/main/java/com/vsmartcard/remotesmartcardreader/app/MainActivity.java
+++ remote-reader/app/src/main/java/com/vsmartcard/remotesmartcardreader/app/MainActivity.java
@@ -59,10 +59,10 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Reader
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
