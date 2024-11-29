frankmorgner/vsmartcard

commit 3c2e54b207a383e5a4b0b3884953a431cd8beb88

ACardEmulator: fixed linter warnings

ACardEmulator/app/src/main/java/com/vsmartcard/acardemulator/emulators/EmulatorSingleton.java
@@ -22,20 +22,12 @@ package com.vsmartcard.acardemulator.emulators;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.XmlResourceParser;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.vsmartcard.acardemulator.Util;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import ACardEmulator.R;
 
@@ -98,32 +90,6 @@ public class EmulatorSingleton {
         LocalBroadcastManager.getInstance(context).sendBroadcast(i);
 
         return rapdu;
-    }
-
-    public static String[] getRegisteredAids(Context context) {
-        List<String> aidList = new ArrayList<>();
-        XmlResourceParser aidXmlParser = context.getResources().getXml(R.xml.aid_list);
-
-        try {
-            while (aidXmlParser.getEventType() != XmlPullParser.END_DOCUMENT) {
-                if (aidXmlParser.getEventType() == XmlPullParser.START_TAG) {
-                    if (aidXmlParser.getName().equals("aid-filter")) {
-                        int aid = aidXmlParser.getAttributeResourceValue(0, -1);
-                        if (aid != -1) {
-                            aidList.add(context.getResources().getString(aid));
-                        }
-                    }
-                }
-
-                aidXmlParser.next();
-            }
-
-            aidXmlParser.close();
-        } catch (XmlPullParserException | IOException e) {
-            Log.e(TAG.substring(0, 23), "Couldn't parse aid xml list.");
-        }
-
-        return aidList.toArray(new String[0]);
     }
 
     public static void deactivate() {

ACardEmulator/app/src/main/AndroidManifest.xml
@@ -21,6 +21,7 @@
         android:theme="@style/AppTheme" >
         <activity
             android:name="com.vsmartcard.acardemulator.MainActivity"
+            android:exported="true"
             android:label="@string/app_name"
             android:theme="@style/AppTheme.NoActionBar"
             >
@@ -33,6 +34,7 @@
 
         <activity
             android:name="com.vsmartcard.acardemulator.SettingsActivity"
+            android:exported="false"
             android:label="@string/action_settings" >
 
             <intent-filter>
@@ -56,12 +58,14 @@
         </service>
 
         <service android:name=".SmartcardProviderService" />
-        <receiver android:name="com.samsung.android.sdk.accessory.RegisterUponInstallReceiver" >
+        <receiver android:name="com.samsung.android.sdk.accessory.RegisterUponInstallReceiver"
+            android:exported="true">
             <intent-filter>
                 <action android:name="com.samsung.accessory.action.REGISTER_AGENT" />
             </intent-filter>
         </receiver>
-        <receiver android:name="com.samsung.android.sdk.accessory.ServiceConnectionIndicationBroadcastReceiver" >
+        <receiver android:name="com.samsung.android.sdk.accessory.ServiceConnectionIndicationBroadcastReceiver"
+            android:exported="true">
             <intent-filter>
                 <action android:name="com.samsung.accessory.action.SERVICE_CONNECTION_REQUESTED" />
             </intent-filter>

ACardEmulator/app/src/main/java/com/vsmartcard/acardemulator/AppCompatPreferenceActivity.java
@@ -4,10 +4,9 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import androidx.annotation.LayoutRes;
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.MenuInflater;
 import android.view.View;
@@ -38,10 +37,7 @@ public abstract class AppCompatPreferenceActivity extends PreferenceActivity {
         return getDelegate().getSupportActionBar();
     }
 
-    public void setSupportActionBar(@Nullable Toolbar toolbar) {
-        getDelegate().setSupportActionBar(toolbar);
-    }
-
+    @NonNull
     @Override
     public MenuInflater getMenuInflater() {
         return getDelegate().getMenuInflater();
@@ -80,7 +76,7 @@ public abstract class AppCompatPreferenceActivity extends PreferenceActivity {
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         getDelegate().onConfigurationChanged(newConfig);
     }

ACardEmulator/app/src/main/java/com/vsmartcard/acardemulator/EmulatorHostApduService.java
@@ -36,11 +36,6 @@ public class EmulatorHostApduService extends HostApduService {
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
     public byte[] processCommandApdu(byte[] capdu, Bundle extras) {
         return EmulatorSingleton.process(this, capdu);
     }

ACardEmulator/app/src/main/java/com/vsmartcard/acardemulator/MainActivity.java
@@ -35,6 +35,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -47,13 +48,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewVPCDStatus;
-    private BroadcastReceiver bReceiver = new BroadcastReceiver() {
+    private final BroadcastReceiver bReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(EmulatorSingleton.TAG)) {
+            if (Objects.requireNonNull(intent.getAction()).equals(EmulatorSingleton.TAG)) {
                 String capdu = intent.getStringExtra(EmulatorSingleton.EXTRA_CAPDU);
                 String rapdu = intent.getStringExtra(EmulatorSingleton.EXTRA_RAPDU);
                 String error = intent.getStringExtra(EmulatorSingleton.EXTRA_ERROR);
@@ -76,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
     private AlertDialog dialog;
     final String PREFS = "ACardEmulatorPrefs";
     final String PREF_LASTVERSION = "last version";
-    private static String SAVED_STATUS = "textViewVPCDStatus";
+    private static final String SAVED_STATUS = "textViewVPCDStatus";
 
     private void showStartupMessage() {
         if (dialog == null)
@@ -94,17 +97,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                 String emulator = SP.getString("emulator", "");
-                if (emulator != getString(R.string.vicc)) {
+                if (!emulator.equals(getString(R.string.vicc))) {
                     Snackbar.make(view, "Scheduled re-installation of all applets...", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                     EmulatorSingleton.destroyEmulator();
@@ -112,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        textViewVPCDStatus = (TextView) findViewById(R.id.textViewLog);
+        textViewVPCDStatus = findViewById(R.id.textViewLog);
         SharedPreferences settings = getSharedPreferences(PREFS, 0);
         if (settings.getInt(PREF_LASTVERSION, 0) != BuildConfig.VERSION_CODE) {
             showStartupMessage();
@@ -161,7 +164,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRestoreInstanceState(Bundle savedInstanceState) {
+    public void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         textViewVPCDStatus.setText(savedInstanceState.getCharSequence(SAVED_STATUS));
     }

ACardEmulator/app/src/main/java/com/vsmartcard/acardemulator/SettingsActivity.java
@@ -20,13 +20,11 @@
 package com.vsmartcard.acardemulator;
 
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -42,6 +40,8 @@ import androidx.appcompat.app.ActionBar;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.vsmartcard.acardemulator.emulators.VICCEmulator;
+
+import java.util.Objects;
 
 import ACardEmulator.BuildConfig;
 import ACardEmulator.R;
@@ -127,7 +127,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                                 : null);
             } else if (preference instanceof SwitchPreference) {
                 SwitchPreference switchPreference = (SwitchPreference) preference;
-                if (stringValue == "true") {
+                if (stringValue.equals("true")) {
                     CharSequence aid = switchPreference.getSwitchTextOn();
                     preference.setSummary("Selectable with AID " + aid);
                 } else {
@@ -174,7 +174,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
      * This fragment shows data and sync preferences only. It is used when the
      * activity is showing a two-pane settings UI.
      */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class VICCPreferenceFragment extends PreferenceFragment {
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -208,16 +207,13 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             Preference internal_nfc = findPreference("internal_nfc");
             internal_nfc.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 public boolean onPreferenceClick(Preference preference) {
-                    if (android.os.Build.VERSION.SDK_INT >= 16) {
-                        startActivity(new Intent(android.provider.Settings.ACTION_NFC_SETTINGS));
-                    } else {
-                        startActivity(new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS));
-                    }
+                    startActivity(new Intent(android.provider.Settings.ACTION_NFC_SETTINGS));
                     return true;
                 }
             });
 
             final Preference gear_nfc = findPreference("gear_nfc");
+            //noinspection ConstantValue
             if (BuildConfig.FLAVOR.equals("fdroid")) {
                 gear_nfc.setSummary("Samsung Gear integration is disabled in F-Droid");
             } else {
@@ -248,12 +244,10 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        switch(requestCode) {
-            case IntentIntegrator.REQUEST_CODE:
-                if (resultCode != RESULT_CANCELED) {
-                    handleScannedURI(Uri.parse(scanResult.getContents()));
-                }
-                break;
+        if (requestCode == IntentIntegrator.REQUEST_CODE) {
+            if (resultCode != RESULT_CANCELED) {
+                handleScannedURI(Uri.parse(scanResult.getContents()));
+            }
         }
     }
 
@@ -273,7 +267,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             getFragmentManager().beginTransaction().replace(android.R.id.content,
                     new VICCPreferenceFragment()).commit();
         } catch (Exception e) {
-            Snackbar.make(this.getCurrentFocus(), "Could not import configuration", Snackbar.LENGTH_LONG)
+            Snackbar.make(Objects.requireNonNull(this.getCurrentFocus()), "Could not import configuration", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
         }
     }

ACardEmulator/app/src/main/java/com/vsmartcard/acardemulator/emulators/Emulator.java
@@ -2,10 +2,10 @@ package com.vsmartcard.acardemulator.emulators;
 
 public interface Emulator {
 
-    public void deactivate();
+    void deactivate();
 
-    public void destroy();
+    void destroy();
 
-    public byte[] process(byte[] commandAPDU);
+    byte[] process(byte[] commandAPDU);
 
 }

ACardEmulator/app/src/main/java/com/vsmartcard/acardemulator/emulators/VICCEmulator.java
@@ -20,10 +20,8 @@ public class VICCEmulator implements Emulator {
     public VICCEmulator(String hostname, int port) {
         if  (socket == null) {
             try {
-                if (android.os.Build.VERSION.SDK_INT > 9) {
-                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                    StrictMode.setThreadPolicy(policy);
-                }
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
                 vpcdConnect(hostname, port);
                 sendPowerOn();
             } catch (IOException e) {
@@ -113,7 +111,7 @@ public class VICCEmulator implements Emulator {
     }
 
     private void vpcdConnect(String hostname, int port) throws IOException {
-        Log.d("", "Connecting to " + hostname + ":" + Integer.toString(port));
+        Log.d("", "Connecting to " + hostname + ":" + port);
         socket = new Socket(InetAddress.getByName(hostname), port);
         outputStream = socket.getOutputStream();
         inputStream = socket.getInputStream();

commit 0c317318e60ea6f23cf6e1df5092a7977fb9ebe1

ACardEmulator: fixed full build
regression from 3c2e54b207a383e5a4b0b3884953a431cd8beb88

ACardEmulator/app/src/main/java/com/vsmartcard/acardemulator/emulators/EmulatorSingleton.java
@@ -22,14 +22,20 @@ package com.vsmartcard.acardemulator.emulators;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.XmlResourceParser;
 import android.preference.PreferenceManager;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.util.Log;
 
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
+import ACardEmulator.R;
 import com.vsmartcard.acardemulator.Util;
 
-import ACardEmulator.R;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmulatorSingleton {
     public static final String TAG = "com.vsmartcard.acardemulator.EmulatorService";
@@ -90,6 +96,32 @@ public class EmulatorSingleton {
         LocalBroadcastManager.getInstance(context).sendBroadcast(i);
 
         return rapdu;
+    }
+
+    public static String[] getRegisteredAids(Context context) {
+        List<String> aidList = new ArrayList<>();
+        XmlResourceParser aidXmlParser = context.getResources().getXml(R.xml.aid_list);
+
+        try {
+            while (aidXmlParser.getEventType() != XmlPullParser.END_DOCUMENT) {
+                if (aidXmlParser.getEventType() == XmlPullParser.START_TAG) {
+                    if (aidXmlParser.getName().equals("aid-filter")) {
+                        int aid = aidXmlParser.getAttributeResourceValue(0, -1);
+                        if (aid != -1) {
+                            aidList.add(context.getResources().getString(aid));
+                        }
+                    }
+                }
+
+                aidXmlParser.next();
+            }
+
+            aidXmlParser.close();
+        } catch (XmlPullParserException | IOException e) {
+            Log.e(TAG.substring(0, 23), "Couldn't parse aid xml list.");
+        }
+
+        return aidList.toArray(new String[0]);
     }
 
     public static void deactivate() {